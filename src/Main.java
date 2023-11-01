f0e10cfee6c2f9d5f102d9fbfd374d94d4f3c4f9
GonsaloLich <kantsaris@gmail.com>
2023-11-01 18:58:09 +0300

    дз готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+
+    public static void task1() {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastname = "Ivanovich";
+        String fullName = firstName + middleName + lastname;
+        System.out.println(fullName);
+    }
+
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета " + fullName.toUpperCase());
+
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String fullName2  =  fullName.replace('ё', 'е');
+        System.out.println(fullName2);
+
+    }
+
 }
